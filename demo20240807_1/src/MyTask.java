7bbc03cb6c59ab1ae7e0e94dfb6bd6ae62107db4
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-08-07 22:26:49 +0200

    first commit

---
+++ demo20240807_1/src/MyTask.java
@@ -0,0 +1,19 @@
+public class MyTask implements Runnable {
+    @Override
+    public void run() {
+
+
+            System.out.println("start Thread 2");
+            for (int i = 100; i < 110; i++) {
+                System.out.println("MyThread2: " + i);
+
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println("end Thread 2");
+        }
+    }
+
---
+++ demo20240807_1/src/MyThread1.java
@@ -0,0 +1,24 @@
+public class MyThread1 extends Thread {
+    private int start;
+    private int end;
+
+    public MyThread1(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("start Thread 1");
+for (int i = 100; i < 110; i++) {
+    System.out.println("MyThread1: " + i);
+
+    try {
+        Thread.sleep(200);
+    } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+    }
+}
+        System.out.println("end Thread 1");
+    }
+}
