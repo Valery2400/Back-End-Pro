b914a55daf1238b5e7c2a5441f515b1314df149b
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-08-20 08:33:47 +0200

    add methods save and deleteById

--- homework_05/books/books/src/main/java/de/ait/books/repository/BookRepositoryJDBCImp.java
+++ homework_05/books/books/src/main/java/de/ait/books/repository/BookRepositoryJDBCImp.java
@@ -4,10 +4,13 @@ import de.ait.books.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class BookRepositoryJDBCImp implements BookRepository {
@@ -35,11 +38,37 @@ public class BookRepositoryJDBCImp implements BookRepository {
 
     @Override
     public Book save(Book book) {
-        return null;
+        if (book.getId() == null) {
+            return create(book);
+            // create new book
+        } else {
+            return update(book);
+        }
+    }
+
+    private Book create(Book book) {
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(datasource)
+                .withTableName("t_book")
+                .usingGeneratedKeyColumns("id");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("title", book.getTitle());
+        params.put("author", book.getAuthor());
+
+        Long generateId = insert.executeAndReturnKey(params).longValue();
+        book.setId(generateId);
+        return book;
+    }
+
+    private Book update(Book book) {
+        String sql = "UPDATE t_book SET title = ?, author = ? WHERE id = ?";
+        int affectedRows = jdbcTemplate.update(sql, book.getTitle(), book.getAuthor(), book.getId());
+        return affectedRows == 1 ? book : null;
     }
 
     @Override
     public void deleteById(int id) {
-
+        String sql = "DELETE FROM t_book WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 }
