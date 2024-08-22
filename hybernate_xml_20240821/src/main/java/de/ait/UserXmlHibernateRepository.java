a4102861cb97f1d31ba92aa5c85c9f6e49044dba
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-08-22 16:31:35 +0200

    add cons05

--- hybernate_xml_20240821/src/main/java/de/ait/UserXmlHibernateRepository.java
+++ hybernate_xml_20240821/src/main/java/de/ait/UserXmlHibernateRepository.java
@@ -2,12 +2,14 @@ package de.ait;
 
 import de.ait.entity.User;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 import java.util.List;
 
 public class UserXmlHibernateRepository {
     private final HibernateConfiguration hibernateConfiguration;
     private final EntityManager entityManager;
+
     public UserXmlHibernateRepository(HibernateConfiguration hibernateConfiguration) {
         this.hibernateConfiguration = hibernateConfiguration;
         entityManager = hibernateConfiguration.getEntityManager();
@@ -15,5 +17,25 @@ public class UserXmlHibernateRepository {
 
     public List<User> findAll() {
         return entityManager.createQuery("from User", User.class).getResultList();
+    }
+
+    public User findById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    public User create(User user) {
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.persist(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Error creating user" + user,e);
+        }
     }
 }
