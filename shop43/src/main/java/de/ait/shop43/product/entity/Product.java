72f58464324c410d385b132cace8a29df1819572
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-08-23 11:50:32 +0200

    create

---
+++ shop43/src/main/java/de/ait/shop43/product/entity/Product.java
@@ -0,0 +1,18 @@
+package de.ait.shop43.product.entity;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Builder    // !!! create pattern Builder
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private boolean active;
+}
