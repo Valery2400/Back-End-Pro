413cac1b650cb1a3c0efe5f6a8b9848cccb0b520
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-08-25 19:00:48 +0200

    Create VocabCycleAPI

---
+++ vocab_cycle/src/main/java/de/ait/vocab_cycle/product/entity/FlashCard.java
@@ -0,0 +1,48 @@
+package de.ait.vocab_cycle.product.entity;
+//Система для повторения слов иностранного языка.
+//Давайте напишем сущность “Карточка слова”: В ней должны быть следующие поля:
+//        ID,
+//        String language (язык оригинала),
+//        String word (слово),
+//        String example (пример использования/фраза),
+//        String translateLanguage (язык перевода),
+//        String translation (перевод),
+//        String group (группа карточек)
+//        Вам необходимо написать API (по примеру того, как сделано на занятии),
+//        которая позволяет отобразить все карточки и добавить новую карточка,
+//        вывести карточку по id.
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@EqualsAndHashCode
+@Builder
+@ToString
+@Setter
+@Getter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FlashCard {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "language")
+    private String language; // язык оригинала
+    @Column(name = "word")
+    private String word; // слово
+    @Column(name = "example")
+    private String example; // пример использования/фраза/example sentence
+    @Column(name = "translateLanguage")
+    private String translateLanguage; // язык перевода
+    @Column(name = "translation")
+    private String translation; // перевод
+    @Column(name = "group")
+    private String group; // группа карточек
+
+}
---
+++ vocab_cycle/src/main/java/de/ait/vocab_cycle/product/repository/FlashCardRepository.java
@@ -0,0 +1,6 @@
+package de.ait.vocab_cycle.product.repository;
+
+import de.ait.vocab_cycle.product.entity.FlashCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlashCardRepository extends JpaRepository<FlashCard, Long> {}
