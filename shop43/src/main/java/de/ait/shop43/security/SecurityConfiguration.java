e0ef7d5ac1e2e254b4ef5ba8568b6cdf46d388bc
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-08-30 11:48:03 +0200

    change SecurityConfiguration

--- shop43/src/main/java/de/ait/shop43/security/SecurityConfiguration.java
+++ shop43/src/main/java/de/ait/shop43/security/SecurityConfiguration.java
@@ -2,9 +2,13 @@ package de.ait.shop43.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
